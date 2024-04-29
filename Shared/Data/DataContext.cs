8cefd983a02e9e2a1eec6f47d2b22e69d3498726
Gimlig <tomek.lutkowski@protonmail.com>
2024-04-29 18:16:31 +0200

    Finished charts

--- Shared/Data/DataContext.cs
+++ Shared/Data/DataContext.cs
@@ -10,9 +10,12 @@ namespace Webapp.Shared.Data
         public DbSet<SavingEntity> Savings => Set<SavingEntity>();
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
         modelBuilder.Entity<SavingEntity>().HasData(
-            new {Id = 1, Category = SavingCategory.Other, Name = "Bułki", Amount = 12.00m, Date = DateTime.Now},
-            new {Id = 2, Category = SavingCategory.Other, Name = "Mydło", Amount = 5.00m, Date = DateTime.Now.AddMinutes(30)},
-            new {Id = 3, Category = SavingCategory.Other, Name = "Plecak", Amount = 1.00m, Date = DateTime.Now.AddHours(1)}
+            new {Id = 1, Category = SavingCategory.Income, Name = "Salary 1", Amount = 590m, Date = DateTime.Now},
+            new {Id = 2, Category = SavingCategory.Income, Name = "Salary 2", Amount = 640m, Date = DateTime.Now.AddMonths(1)},
+            new {Id = 3, Category = SavingCategory.Savings, Name = "Saving 1", Amount = 20.00m, Date = DateTime.Now.AddHours(1)},
+            new {Id = 4, Category = SavingCategory.Savings, Name = "Saving 2", Amount = 35.00m, Date = DateTime.Now.AddMonths(1).AddDays(1)},
+            new {Id = 5, Category = SavingCategory.Other, Name = "Books", Amount = 62.00m, Date = DateTime.Now.AddHours(2)},
+            new {Id = 6, Category = SavingCategory.Other, Name = "Toys", Amount = 93.00m, Date = DateTime.Now.AddMonths(1).AddDays(2)}
         );
     }
     }
